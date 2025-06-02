b90f79a481796caa61a3ce8e1c447ec2eb6ecc1e
AnnLinnden <annlinnden@gmail.com>
2025-06-02 17:43:10 +0600

    Настроила логирование и запуск приложения

--- wallet/main.py
+++ wallet/main.py
@@ -1,7 +1,27 @@
+import logging
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from wallet.config import database
 
-app = FastAPI()
+handler_console = logging.StreamHandler()
+handler_file = logging.FileHandler('app.log')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[handler_file, handler_console])
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager  # .on_event("startup") устарел
+async def lifespan(app: FastAPI):
+    logger.info('Запуск приложения')
+    try:
+        await database.create_tables()
+        logger.info('Таблицы готовы к работе')
+    except Exception as e:
+        logger.error(f'Ошибка при создании таблиц: {e}')
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/api/v1/wallets/{WALLET_UUID}")
@@ -12,9 +32,11 @@ async def get_wallet_info(wallet_uuid: str) -> dict:
 
 @app.post("api/v1/wallets/{WALLET_UUID}/deposit")
 async def deposit_to_wallet(wallet_uuid: str, amount: int):
-    pass
+    database.increase_wallet_balance(wallet_uuid, amount)
+    return f'Баланс кошелька {wallet_uuid} увеличен на {amount}'
 
 
 @app.post("api/v1/wallets/{WALLET_UUID}/withdraw")
 async def withdraw_from_wallet(wallet_uuid: str, amount: int):
-    pass
+    database.decrease_wallet_balance(wallet_uuid, amount)
+    return f'Баланс кошелька {wallet_uuid} уменьшен на {amount}'
