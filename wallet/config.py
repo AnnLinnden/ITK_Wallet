6abb1d30b9b984365754bb849100399b8a89e4fc
AnnLinnden <annlinnden@gmail.com>
2025-06-02 17:50:41 +0600

    Обносила базовые настройки

--- wallet/config.py
+++ wallet/config.py
@@ -1,4 +1,3 @@
-import logging
 from os import getenv
 from dotenv import load_dotenv
 from wallet.db.storage import DatabaseManager
@@ -7,11 +6,5 @@ load_dotenv()
 database_url = getenv('DATABASE_URL')
 database = DatabaseManager(database_url)
 
-handler_console = logging.StreamHandler()
-handler_file = logging.FileHandler('app.log')
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[handler_file, handler_console])
-logger = logging.getLogger(__name__)
 
 
