7808451bb9af29fb1761af4ca57eb0b7d41e1593
AnnLinnden <annlinnden@gmail.com>
2025-06-18 13:14:10 +0600

    Мелкий фикс

--- wallet/db/storage.py
+++ wallet/db/storage.py
@@ -7,9 +7,9 @@ from .models.sql_models import Wallet
 class DatabaseManager:
     def __init__(self, database_url):
         self.engine = create_async_engine(database_url)
-        self.async_session = async_sessionmaker(self.engine,
-                                                class_=AsyncSession,
-                                                expire_on_commit=False)
+        self.async_session = async_sessionmaker(
+            self.engine, class_=AsyncSession, expire_on_commit=False
+        )
         self.wallet = Wallet
 
     async def create_wallet(self, wallet_uuid: str, balance: int) -> None:
@@ -22,7 +22,7 @@ class DatabaseManager:
         async with self.async_session() as session:
             wallet_obj = await session.get(self.wallet, wallet_uuid)
             if wallet_obj is None:
-                raise Exception(f'Кошелек {wallet_uuid} не найден')
+                raise Exception(f"Кошелек {wallet_uuid} не найден")
             else:
                 return wallet_obj.balance
 
@@ -31,7 +31,8 @@ class DatabaseManager:
             wallet_obj = await session.execute(
                 select(self.wallet)
                 .where(self.wallet.uuid == wallet_uuid)
-                .with_for_update())
+                .with_for_update()
+            )
             result = wallet_obj.scalars().one_or_none()
             if not result:
                 await self.create_wallet(wallet_uuid, amount)
@@ -44,21 +45,27 @@ class DatabaseManager:
             wallet_obj = await session.execute(
                 select(self.wallet)
                 .where(self.wallet.uuid == wallet_uuid)
-                .with_for_update())
+                .with_for_update()
+            )
             result = wallet_obj.scalars().one_or_none()
             if not result:
-                await self.create_wallet(wallet_uuid, 0)  # создаем кошелек в любом случае
-                raise Exception(f'Кошелек {wallet_uuid} имеет нулевой баланс. Не хватает средств для вывода')
+                await self.create_wallet(
+                    wallet_uuid, 0
+                )  # создаем кошелек в любом случае
+                raise Exception(
+                    f"Кошелек {wallet_uuid} имеет нулевой баланс. Не хватает средств для вывода"
+                )
             if result.balance >= amount:
                 result.balance -= amount
                 await session.commit()
             else:
-                raise Exception(f'Недостаточно средств на кошельке {wallet_uuid}')
+                raise Exception(f"Недостаточно средств на кошельке {wallet_uuid}")
 
     async def check_tables_exist(self) -> bool:
         async with self.engine.connect() as conn:
             result_for_wallet_table = await conn.execute(
-                text("SELECT to_regclass('public.wallet')"))  # используем стандартную схему PostgreSQL - public
+                text("SELECT to_regclass('public.wallet')")
+            )  # используем стандартную схему PostgreSQL - public
             table_wallet_exists = result_for_wallet_table.scalar()
             return table_wallet_exists
 
@@ -67,6 +74,3 @@ class DatabaseManager:
         if not tables_exist:
             async with self.engine.begin() as conn:
                 await conn.run_sync(SQLModel.metadata.create_all)
-
-
-
--- wallet/main.py
+++ wallet/main.py
@@ -5,23 +5,26 @@ from wallet.config import get_database
 from wallet.db.models.schemas import Amount
 
 handler_console = logging.StreamHandler()
-handler_file = logging.FileHandler('app.log')
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[handler_file, handler_console])
+handler_file = logging.FileHandler("app.log")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[handler_file, handler_console],
+)
 logger = logging.getLogger(__name__)
 
 
 database = get_database()[0]
 
+
 @asynccontextmanager  # .on_event("startup") устарел
 async def lifespan(app: FastAPI):
-    logger.info('Запуск приложения')
+    logger.info("Запуск приложения")
     try:
         await database.create_tables()
-        logger.info('Таблицы готовы к работе')
+        logger.info("Таблицы готовы к работе")
     except Exception as e:
-        logger.error(f'Ошибка при создании таблиц: {e}')
+        logger.error(f"Ошибка при создании таблиц: {e}")
     yield
 
 
@@ -34,18 +37,22 @@ async def get_wallet_info(wallet_uuid: str) -> dict:
         wallet_balance = await database.get_wallet_balance(wallet_uuid)
         return {"uuid": wallet_uuid, "balance": wallet_balance}
     except Exception as e:
-        raise HTTPException(status_code=404, detail=f'Не удалось получить информацию о кошельке: {e}')
+        raise HTTPException(
+            status_code=404, detail=f"Не удалось получить информацию о кошельке: {e}"
+        )
 
 
 @app.post("/api/v1/wallets/{wallet_uuid}/deposit")
 async def deposit_to_wallet(wallet_uuid: str, amount: Amount):
     try:
         await database.increase_wallet_balance(wallet_uuid, amount.amount)
-        return f'Баланс кошелька {wallet_uuid} увеличен на {amount.amount}'  # amount.amount для корректного ответа
+        return f"Баланс кошелька {wallet_uuid} увеличен на {amount.amount}"  # amount.amount для корректного ответа
         # из модели pydantic
     except Exception as e:
-        logger.error(f'Ошибка при увеличении баланса: {e}')
-        raise HTTPException(status_code=500, detail=f'Ошибка при увеличении баланса: {e}')  # 500 - потому что,
+        logger.error(f"Ошибка при увеличении баланса: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Ошибка при увеличении баланса: {e}"
+        )  # 500 - потому что,
         # скорее всего, проблема на нашей стороне, а не у пользователя
 
 
@@ -53,12 +60,15 @@ async def deposit_to_wallet(wallet_uuid: str, amount: Amount):
 async def withdraw_from_wallet(wallet_uuid: str, amount: Amount):
     try:
         await database.decrease_wallet_balance(wallet_uuid, amount.amount)
-        return f'Баланс кошелька {wallet_uuid} уменьшен на {amount.amount}'
+        return f"Баланс кошелька {wallet_uuid} уменьшен на {amount.amount}"
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f'Ошибка при уменьшении баланса: {e}')  # а тут скорее проблема
+        raise HTTPException(
+            status_code=400, detail=f"Ошибка при уменьшении баланса: {e}"
+        )  # а тут скорее проблема
         # с запросом, то есть на стороне пользователя
 
 
 if __name__ == "__main__":  # не использовать в продакшене!
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
